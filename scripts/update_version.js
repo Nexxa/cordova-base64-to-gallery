3d740e8e633307a92de5c19b0430b75199bf2171
StefanoMagrassi <stefano.magrassi@gmail.com>
2016-01-20 17:58:18 +0100

    removed libxmljs and back to good ol' regex

--- scripts/update_version.js
+++ scripts/update_version.js
@@ -2,18 +2,28 @@
 
 // Modules
 var fs     = require('fs');
-var libxml = require('libxmljs');
+var logger = require('nodemsg');
 var pkg    = require('../package.json');
 
 // CONSTS
 var CONFIG_FILE = 'plugin.xml';
+var PLUGIN_ID   = 'cordova-base64-to-gallery';
+var ERROR_MSG   = 'No "version" attribute found - Please check '+ CONFIG_FILE +' ("version" tag must follow "id" tag)';
+var REGEXP      = '(id="' + PLUGIN_ID + '" )(version="\\d+[.]\\d+[.]\\d+")';
 
-var version       = pkg.version;
-var configContent = fs.readFileSync(CONFIG_FILE);
-var configXML     = libxml.parseXmlString(configContent);
-var configVersion = configXML.root().attr('version');
+// Logic
+var version = pkg.version;
+var regex   = new RegExp(REGEXP);
+var config  = fs.readFileSync(CONFIG_FILE, { encoding: 'utf8'});
+
+// Exit if version tag not found
+if (!regex.test(config)) {
+  logger.error(ERROR_MSG);
+
+  process.exit(1);
+}
 
 // Set version
-configVersion.value(version);
+config = config.replace(regex, '$1version="' + version + '"');
 
-fs.writeFileSync(CONFIG_FILE, configXML.toString());
+fs.writeFileSync(CONFIG_FILE, config);
